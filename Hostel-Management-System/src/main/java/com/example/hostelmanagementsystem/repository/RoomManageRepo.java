49c125495a3342c45764d9333907b3cc33705f1f
VinulVinsura <155805055+VinulVinsura@users.noreply.github.com>
2024-12-31 15:59:36 +0530

    get-room-byId and update-room

--- Hostel-Management-System/src/main/java/com/example/hostelmanagementsystem/repository/RoomManageRepo.java
+++ Hostel-Management-System/src/main/java/com/example/hostelmanagementsystem/repository/RoomManageRepo.java
@@ -4,8 +4,11 @@ import com.example.hostelmanagementsystem.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RoomManageRepo extends JpaRepository<Room,Long> {
 
     boolean existsByRoomId(String roomId);
+    Room findByRoomId(String roomId);
 }
--- Hostel-Management-System/src/main/java/com/example/hostelmanagementsystem/service/impl/admin/RoomManageServiceImpl.java
+++ Hostel-Management-System/src/main/java/com/example/hostelmanagementsystem/service/impl/admin/RoomManageServiceImpl.java
@@ -13,6 +13,7 @@ import org.modelmapper.TypeToken;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -51,6 +52,39 @@ public class RoomManageServiceImpl implements RoomManageService {
             List<ResponseRoomDto> responseRoomDtoList=modelMapper.map(roomList,
                     new TypeToken<List<ResponseRoomDto>>(){}.getType());
             return new ResponseDto(00,responseRoomDtoList);
+
+        }catch (Exception ex){
+            return new ResponseDto(02,ex.getMessage());
+        }
+    }
+
+    @Override
+    public ResponseDto getRoomByRoomId(String roomId) {
+        try {
+            if(roomManageRepo.existsByRoomId(roomId)){
+                Room room = roomManageRepo.findByRoomId(roomId);
+                return new ResponseDto(00,modelMapper.map(room, ResponseRoomDto.class));
+            }else {
+                return new ResponseDto(01,"Invalid Room ID");
+            }
+        }catch (Exception ex) {
+            return new ResponseDto(02,ex.getMessage());
+        }
+    }
+
+    @Override
+    public ResponseDto updateRoom(Long id, RoomDto updateRoomDto) {
+        try {
+            Optional<Room> room = roomManageRepo.findById(id);
+            room.get().setRoomId(updateRoomDto.getRoomId());
+            room.get().setRoom_capacity(updateRoomDto.getRoom_capacity());
+            room.get().setFilled_capacity(updateRoomDto.getFilled_capacity());
+            room.get().setRemark(updateRoomDto.getRemark());
+            room.get().setHostelDetail(HostelDetail.builder().id(updateRoomDto.getHostel_id()).build());
+            Room updatedRoom = roomManageRepo.save(room.get());
+
+            return new ResponseDto(00,modelMapper.map(updatedRoom, ResponseRoomDto.class));
+
 
         }catch (Exception ex){
             return new ResponseDto(02,ex.getMessage());
